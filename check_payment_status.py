6de619d487d34c6e65cf611611be9fc1c077612a
Dev Pancholi <112951112+Devpancholi04@users.noreply.github.com>
2024-07-04 00:33:39 +0530

    Add files via upload

--- check_payment_status.py
+++ check_payment_status.py
@@ -1,17 +1,12 @@
 import requests
-import json
-import time
 import hashlib
-import pay2 as p
 
-
-#transactionId = p.merchantTransactionId
 merchantId = "PGTESTPAYUAT86"
 salt_key = "96434309-7796-489d-8924-ab56988a6076"
 salt_index = 1
 
 def generate_xverify(merchantTransactionId,salt_key, salt_index):
-    #sha256_data = f'/v3/transaction/{merchantId}/{transactionId}/status{salt_key}'
+    global sha256_data
     sha256_data = f'/pg/v1/status/{merchantId}/{merchantTransactionId}{salt_key}'
     print(f"\ndata to hash = {sha256_data}")
     
@@ -22,8 +17,8 @@ def generate_xverify(merchantTransactionId,salt_key, salt_index):
     print(f"x-verify = {x_verify} ")
     return x_verify
 
-def check_status(payload, merchantId, transactionId, salt_key):    
-    api = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/status/{merchantId}/{merchantTransactionId}"
+def check_status(merchantId, merchantTransactionId, salt_key):    
+    api = f"https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/status/{merchantId}/{merchantTransactionId}"
     
     x_verify = generate_xverify(transactionId, salt_key, salt_index)
     
@@ -34,15 +29,11 @@ def check_status(payload, merchantId, transactionId, salt_key):
         "X-MERCHANT-ID": merchantId
         }
     
-    respond= requests.get(api,json = payload, headers=headers)
-    respond.raise_for_status()
-    print(respond.text)
+    respond= requests.get(api ,headers=headers)  
+    print(respond.text)    
     
-#print(transactionId)
-
-#print(api)
-
 if __name__ == '__main__':
+    
     transactionId = 'TXN7641011dev125421456987453'
     
-    check_status(p.payload, merchantId, transactionId, salt_key)
+    check_status(merchantId, transactionId, salt_key)
